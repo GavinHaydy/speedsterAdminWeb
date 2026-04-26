b9681414cef4806a210167eecb62c30a9ddb6b10
rui.liu <3311579771@qq.com>
2026-04-26 16:14:52 +0800

    feat: update antd version

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
+
 import { router } from './router';
 import { store } from './store';
+
+import '@ant-design/v5-patch-for-react-19';
 // import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
