e66938b612cae9b7f5d8afc71ec9db48d35ecc20
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-06-19 21:01:40 +0800

    feat: list

--- src/pages/Permission/index.tsx
+++ src/pages/Permission/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { Tree, type TreeDataNode, type TreeProps } from 'antd';
+
 import { permissionList } from '@/api/generated';
 import type { PermissionListResult } from '@/types/generated';
 
@@ -7,16 +9,33 @@ export const Permission = () => {
   const [permissions, setPermissions] = useState<PermissionListResult>({ total: 0, list: [] });
 
   useEffect(() => {
-    const fetchPermissions = async () => {
-      try {
-        const res = await permissionList({ name: '' });
-        setPermissions(res);
-      } catch (error) {
-        console.error('获取权限列表失败:', error);
-      }
+    const fetchData = async () => {
+      const res = await permissionList({ name: '' });
+      setPermissions(res);
     };
-
-    fetchPermissions().then();
+    fetchData().then();
   }, []);
-  return <div>{permissions.list.map((item) => item.name)}</div>;
+
+  const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
+
+  const onCheck: TreeProps['onCheck'] = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
+
+  return (
+    <Tree
+      checkable
+      onSelect={onSelect}
+      onCheck={onCheck}
+      fieldNames={{
+        title: 'name',
+        key: 'id',
+        children: 'children',
+      }}
+      disabled={true}
+      treeData={permissions.list as unknown as TreeDataNode[]}
+    />
+  );
 };
