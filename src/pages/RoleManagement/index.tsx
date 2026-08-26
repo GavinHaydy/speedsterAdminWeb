8afd0394f047b30c948c1ca2049fffee3a892955
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-08-26 22:42:34 +0800

    feat: search component

--- src/pages/RoleManagement/index.tsx
+++ src/pages/RoleManagement/index.tsx
@@ -1,3 +1,33 @@
+import { Form, Input } from 'antd';
+
+import type { RoleListParams } from '@/types/generated';
+
 export const RoleManagement: React.FC = () => {
-  return <>role list</>;
+  const [searchForm] = Form.useForm();
+
+  return (
+    <>
+      <Form
+        form={searchForm}
+        layout="inline"
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 16,
+        }}
+      >
+        <Form.Item<RoleListParams> name="rolename" label="角色名">
+          <Input allowClear />
+        </Form.Item>
+
+        <Form.Item<RoleListParams> name="code" label="角色编码">
+          <Input allowClear />
+        </Form.Item>
+
+        <Form.Item<RoleListParams> name="status" label="状态">
+          <Input allowClear />
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
